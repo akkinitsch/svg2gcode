eb465a7b8acb8883645337c6e8a341fad95835cb
Akki Nitsch <akki@akki-n.de>
2020-01-30 06:49:17 +0100

    Read input from inputfile. WRite output to outputfile. Deactivated scaling. Refactoring: moved some functionality in own methods.

--- svg2gcode.py
+++ svg2gcode.py
@@ -6,10 +6,10 @@ import shapes as shapes_pkg
 from shapes import point_generator
 from config import *
 
-def generate_gcode():
+def generate_gcode(filename, outputfile_name):
     svg_shapes = set(['rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon', 'path'])
-    
-    tree = ET.parse(sys.stdin)
+
+    tree = ET.parse(filename)
     root = tree.getroot()
     
     width = root.get('width')
@@ -23,13 +23,14 @@ def generate_gcode():
         print "Unable to get width and height for the svg"
         sys.exit(1)
 
-    width = float(width)
-    height = float(height)
+    width = checkAndReturnMeasurementInMillimeter(width)
+    height = checkAndReturnMeasurementInMillimeter(height)
 
-    scale_x = bed_max_x / max(width, height)
-    scale_y = bed_max_y / max(width, height)
+    #scale_x = bed_max_x / max(width, height)
+    #scale_y = bed_max_y / max(width, height)
 
-    print preamble 
+    output_file = open(outputfile_name, "w")
+    write_gcodeline(output_file, preamble)
     
     for elem in root.iter():
         
@@ -45,17 +46,38 @@ def generate_gcode():
             m = shape_obj.transformation_matrix()
 
             if d:
-                print shape_preamble 
+                write_gcodeline(output_file, shape_preamble)
                 p = point_generator(d, m, smoothness)
+                move_to_startposition_shape_done = False
                 for x,y in p:
-                    if x > 0 and x < bed_max_x and y > 0 and y < bed_max_y:  
-                        print "G1 X%0.1f Y%0.1f" % (scale_x*x, scale_y*y) 
-                print shape_postamble
+                    #if x > 0 and x < bed_max_x and y > 0 and y < bed_max_y:  
+                    #    print "G1 X%0.1f Y%0.1f" % (scale_x*x, scale_y*y) 
+                    #print "G1 X%0.1f Y%0.1f" % (scale_x*x - bed_max_x/2.0, scale_y*y - bed_max_y/2.0)
+                    gcode_line ="G1 X%0.1f Y%0.1f" % (x - (bed_max_x/2.0), y - (bed_max_y/2.0))
+                    write_gcodeline(output_file, gcode_line)
+                    if not move_to_startposition_shape_done:
+                        gcode_line_zposition_plotting = "G1 Z%0.1f" %(z_position_plotting)
+                        write_gcodeline(output_file, gcode_line_zposition_plotting)
+                        move_to_startposition_shape_done = True
 
-    print postamble 
+                write_gcodeline(output_file, shape_postamble)
+
+    write_gcodeline(output_file, postamble)
+
+def checkAndReturnMeasurementInMillimeter(value):
+    if not value[-2:] == "mm":
+        #TODO: Exception werfen
+        pass
+    else:
+        result = value.split("mm")
+        return float(result[0])
+
+def write_gcodeline(outputfile, line, consoleprint=True):
+    outputfile.write(line + "\n")
+    if consoleprint:
+        print line
 
 if __name__ == "__main__":
-    generate_gcode()
-
+    generate_gcode("test.svg", "test.gcode")
 
 
